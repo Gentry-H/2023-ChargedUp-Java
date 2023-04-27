7bf6b94e711c2dc2cd163bd96b697daa308364a9
Gentry House <gentry@housefam.org>
2023-04-26 21:13:59 -0400

    Initial Commit, Non-functional, everything but autonomous done

---
+++ src/main/java/frc/robot/subsystems/BC_FalconFX.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BC_FalconFX extends SubsystemBase {
+    public BC_FalconFX() {}
+
+
+}
